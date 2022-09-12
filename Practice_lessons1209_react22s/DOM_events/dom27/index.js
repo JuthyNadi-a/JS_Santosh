197aae670402cd617d4fbcde793c25610a6700e0
Nadia Jahan Juthy <juthynadia@gmail.com>
2022-09-12 14:41:30 +0300

    dom tasks added

--- Practice_lessons1209_react22s/DOM_events/dom27/index.js
+++ Practice_lessons1209_react22s/DOM_events/dom27/index.js
@@ -1,1 +1,5 @@
 // TODO: log the button's textContent when it's clicked
+let clickBtn = document.querySelector('#my-button');
+clickBtn.addEventListener('click', function() {
+    console.log(clickBtn.innerHTML)
+})
--- Practice_lessons1209_react22s/DOM_inter/dom24/index.js
+++ Practice_lessons1209_react22s/DOM_inter/dom24/index.js
@@ -1,1 +1,6 @@
 // TODO: when the user clicks on '#menu-sidebar' => toggle the class 'show' on '#app-sidebar'
+let sideBar = document.querySelector('#app-sidebar');
+let menuBar = document.querySelector('#menu-sidebar');
+menuBar.addEventListener('click', function() {
+    sideBar.classList.toggle('show');
+});
